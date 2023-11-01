fe6d80032cdcb90bc6b95f8889e6f60d32401df7
MehreenI <aiyanakhan2772@gmail.com>
2023-11-01 20:20:00 +0500

    Updated

--- app/src/main/java/com/example/myapplication/SaleFragment.java
+++ app/src/main/java/com/example/myapplication/SaleFragment.java
@@ -1,148 +1,101 @@
-package com.example.myapplication;
-import android.Manifest;
-import android.content.ContentResolver;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Bundle;
-import android.provider.MediaStore;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.webkit.MimeTypeMap;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
+package com.example.myapplication
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.Fragment;
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.MimeTypeMap
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-import android.Manifest;
-import android.content.ContentResolver;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Bundle;
-import android.provider.MediaStore;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.webkit.MimeTypeMap;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.Toast;
-import androidx.annotation.Nullable;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.Fragment;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-
-public class SaleFragment extends Fragment {
-    private static final int GALLERY_REQUEST_CODE = 1000;
-    private Uri imageUri;
-    private ImageButton imgGallery;
-    private Button btn;
-    private Button upload_Data;
-    private StorageReference mStorageReference;
-    private DatabaseReference mDataBaseReference;
-
-    private EditText bookNameEditText;
-    private EditText bookPriceEditText;
-
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_sale, container, false);
-
-        imgGallery = view.findViewById(R.id.imageButton);
-//        btn = view.findViewById(R.id.btn);
-        upload_Data = view.findViewById(R.id.uploadData);
-
-        bookNameEditText = view.findViewById(R.id.bookNameEditText);
-        bookPriceEditText = view.findViewById(R.id.price);
-
-        mStorageReference = FirebaseStorage.getInstance().getReference("uploads");
-        mDataBaseReference = FirebaseDatabase.getInstance().getReference("uploads");
-
-        imgGallery.setOnClickListener(v -> pickImageFromGallery());
-        upload_Data.setOnClickListener(v -> uploadFile());
-
-        return view;
+class SaleFragment : Fragment() {
+    private var imageUri: Uri? = null
+    private var imgGallery: ImageButton? = null
+    private val btn: Button? = null
+    private var upload_Data: Button? = null
+    private var mStorageReference: StorageReference? = null
+    private var mDataBaseReference: DatabaseReference? = null
+    private var bookNameEditText: EditText? = null
+    private var bookPriceEditText: EditText? = null
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_sale, container, false)
+        imgGallery = view.findViewById(R.id.imageButton)
+        //        btn = view.findViewById(R.id.btn);
+        upload_Data = view.findViewById(R.id.uploadData)
+        bookNameEditText = view.findViewById(R.id.bookNameEditText)
+        bookPriceEditText = view.findViewById(R.id.price)
+        mStorageReference = FirebaseStorage.getInstance().getReference("uploads")
+        mDataBaseReference = FirebaseDatabase.getInstance().getReference("uploads")
+        imgGallery.setOnClickListener(View.OnClickListener { v: View? -> pickImageFromGallery() })
+        upload_Data.setOnClickListener(View.OnClickListener { v: View? -> uploadFile() })
+        return view
     }
 
-    private void pickImageFromGallery() {
-        Intent galleryIntent = new Intent(Intent.ACTION_PICK);
-        galleryIntent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE);
+    private fun pickImageFromGallery() {
+        val galleryIntent = Intent(Intent.ACTION_PICK)
+        galleryIntent.data = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE)
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == GALLERY_REQUEST_CODE && resultCode == AppCompatActivity.RESULT_OK && data != null) {
-            imageUri = data.getData();
-            imgGallery.setImageURI(imageUri);
+            imageUri = data.data
+            imgGallery!!.setImageURI(imageUri)
         }
     }
 
-    public void uploadFile() {
+    fun uploadFile() {
         if (imageUri != null) {
-            StorageReference fileReference = mStorageReference.child(System.currentTimeMillis() + "." + getFileExtension(imageUri));
-
-            fileReference.putFile(imageUri)
-                    .addOnSuccessListener(taskSnapshot -> {
-                        fileReference.getDownloadUrl().addOnSuccessListener(uri -> {
-                            String downloadUrl = uri.toString();
-                            String bookName = bookNameEditText.getText().toString();
-                            String bookPrice = bookPriceEditText.getText().toString();
-
-                            ImageUpload upload = new ImageUpload(bookName, bookPrice, downloadUrl);
-                            String uploadId = mDataBaseReference.push().getKey();
-                            mDataBaseReference.child(uploadId).setValue(upload);
-
-                        });
-                    })
-                    .addOnFailureListener(e -> {
-                        Toast.makeText(getActivity(), "Fail to Upload Image", Toast.LENGTH_SHORT).show();
-                    });
+            val fileReference = mStorageReference!!.child(
+                System.currentTimeMillis().toString() + "." + getFileExtension(
+                    imageUri!!
+                )
+            )
+            fileReference.putFile(imageUri!!)
+                .addOnSuccessListener { taskSnapshot: UploadTask.TaskSnapshot? ->
+                    fileReference.downloadUrl.addOnSuccessListener { uri: Uri ->
+                        val downloadUrl = uri.toString()
+                        val bookName = bookNameEditText!!.text.toString()
+                        val bookPrice = bookPriceEditText!!.text.toString()
+                        val upload = ImageUpload(bookName, bookPrice, downloadUrl)
+                        val uploadId = mDataBaseReference!!.push().key
+                        mDataBaseReference!!.child(uploadId!!).setValue(upload)
+                    }
+                }
+                .addOnFailureListener { e: Exception? ->
+                    Toast.makeText(
+                        activity,
+                        "Fail to Upload Image",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
         }
     }
 
-    private String getFileExtension(Uri uri) {
-        ContentResolver contentResolver = getActivity().getContentResolver();
-        MimeTypeMap mime = MimeTypeMap.getSingleton();
-        return mime.getExtensionFromMimeType(contentResolver.getType(uri));
+    private fun getFileExtension(uri: Uri): String? {
+        val contentResolver = activity!!.contentResolver
+        val mime = MimeTypeMap.getSingleton()
+        return mime.getExtensionFromMimeType(contentResolver.getType(uri))
+    }
+
+    companion object {
+        private const val GALLERY_REQUEST_CODE = 1000
     }
 }
